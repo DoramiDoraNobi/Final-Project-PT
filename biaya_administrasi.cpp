607f275082609caf67b64c3c02919fb17f98b796
Mikail Yuddha Wibowo <57121735+DoramiDoraNobi@users.noreply.github.com>
2022-07-01 17:46:56 +0700

    Update biaya_administrasi.cpp
    Menambahkan Functions

--- biaya_administrasi.cpp
+++ biaya_administrasi.cpp
@@ -2,13 +2,62 @@
 
 using namespace std;
 
+string nama, nim, user_fakul, fakul[3];
+int fakultas, prodi, spp_tetap, sks, spp_vbl, totbay;
+bool kelas;
+
+int Percabangan(int fakul_kuliah){
+	//Code If ELse
+	if(fakul_kuliah == 1){
+		user_fakul = fakul[0];
+		cout<<"Kamu adalah Anggota dari "<<user_fakul<<endl;
+		cout<<"Apakah Anda Kelas International atau Kelas Reguler. Ketik 1 Untuk Kelas International dan Ketik 2 Untuk Kelas Reguler"<<endl;
+		cin>>kelas;
+		if(kelas == 1 ){
+			spp_tetap = 2550000;
+			spp_vbl = 180000;
+		}
+		else{
+			spp_tetap = 2075000;
+			spp_vbl = 140000;
+		}
+	}
+	else if(fakul_kuliah == 2){
+		user_fakul = fakul[1];
+		cout<<"Kamu adalah Anggota dari "<<user_fakul<<endl;
+		spp_tetap == 1475000;
+		spp_vbl == 140000;
+	}
+	else if(fakul_kuliah == 3){
+		user_fakul = fakul[2];
+		cout<<"Kamu adalah Anggota dari "<<user_fakul<<endl;
+		spp_tetap = 2075000;
+		spp_vbl = 140000;
+	}
+	else{
+		cout<<"Mohon Masukkan Input yang sudah disediakan di atas"<<endl;
+	}
+}
+
+
+
+
+//Fungsi Menghitung Total Bayar
+int Perhitungan(int spp_ttp, int spp_variabel, int sks){
+	int spp_totVbl = spp_variabel * sks;
+	int tot_bay = spp_ttp + spp_totVbl;
+	cout<<"Total SPP Tetap : "<<spp_ttp<<endl;
+	cout<<"Total SPP Variabel : "<<spp_variabel<<endl;
+	cout<<"Total Yang Harus Dibayarkan: "<<tot_bay<<endl;
+	return tot_bay;
+}
+
 int main(){
-	string nama, nim, user_fakul, fakul[3];
+	
 	fakul[0] = "Fakultas Ilmu Komputer";
 	fakul[1] = "Fakultas Ekonomi dan Sosial";
 	fakul[2] = "Fakultas Sains dan Teknologi";
-	int fakultas, prodi, spp_tetap, sks, spp_vbl, totbay;
-	bool kelas;
+	
 	
 	// Code Input Nama dan Nim 
 	cout<<"Masukkan NIM Anda : "<<endl;
@@ -20,49 +69,14 @@ int main(){
 	cout<<"Ketik 2 untuk Fakultas Ekonomi dan Sosial"<<endl;
 	cout<<"Ketik 3 untuk Fakultas Sains dan Teknologi"<<endl;
 	cin>>fakultas;
-	
-	//Code If ELse
-	if(fakultas == 1){
-		user_fakul = fakul[0];
-		cout<<"Kamu adalah Anggota dari "<<user_fakul<<endl;
-		cout<<"Apakah Anda Kelas International atau Kelas Reguler. Ketik 1 Untuk Kelas International dan Ketik 2 Untuk Kelas Reguler"<<endl;
-		cin>>kelas;
-		if(kelas == 1 ){
-			spp_tetap = 2550000;
-			spp_vbl = 180000*sks;
-		}
-		else{
-			spp_tetap = 2075000;
-			spp_vbl = 140000*sks;
-		}
-	}
-	else if(fakultas == 2){
-		user_fakul = fakul[1];
-		cout<<"Kamu adalah Anggota dari "<<user_fakul<<endl;
-		spp_tetap == 1475000;
-		spp_vbl == 140000;
-	}
-	else if(fakultas == 3){
-		user_fakul = fakul[2];
-		cout<<"Kamu adalah Anggota dari "<<user_fakul<<endl;
-		spp_tetap = 2075000;
-		spp_vbl = 140000*sks;
-	}
-	else{
-		cout<<"Mohon Masukkan Input yang sudah disediakan di atas"<<endl;
-	}
+	cout<<Percabangan(fakultas);
 	
 	cout<<"Masukkan SKS : "<<endl;
 	cin>>sks;
-	totbay = spp_tetap + spp_vbl;
 	cout<<"Administrasi Mahasiswa Amikom"<<endl; 
 	cout<<user_fakul<<endl;
-	
 	cout<<"Nama Mahasiswa : "<<nama<<endl;
-	cout<<"NIM Mahasiswa : "<<nim<<endl;
-	cout<<"Jumlah SPP Tetap yang harus di bayarkan : Rp."<<spp_tetap<<endl;
-	cout<<"jumlah SPP Variabel yang harus di bayarkan : Rp."<<spp_vbl<<endl;
-	cout<<"Maka Total Bayar : Rp."<<totbay;
+	cout<<Perhitungan(spp_tetap, spp_vbl, sks);
 	
 	
 }
